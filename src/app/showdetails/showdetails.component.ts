6a24f7d1cf582a7da9483645a504f27c029ba954
fiyaspk <fiyaspk1820@gmail.com>
2021-07-22 13:14:53 +0530

    commit3

--- src/app/showdetails/showdetails.component.ts
+++ src/app/showdetails/showdetails.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataserviceService } from '../dataservice.service';
-
+import { Register } from '../registration/registration.model';
 @Component({
   selector: 'app-showdetails',
   templateUrl: './showdetails.component.html',
@@ -28,6 +28,37 @@ export class ShowdetailsComponent implements OnInit {
   
     })
   }
+  public show:boolean=false;
+  public buttonName:any='Show';
+  upd=new Register()
+  updt(){
+    console.log("check")
+    this.pass.update(this.upd).subscribe(result=>{
+    console.log(result)
+    this.showdata()
+    alert(result)
+    window.location.reload();
+    
+  })
+ }
 
+
+
+ dt:any
+ updatedata(asd:any){
+  this.show = !this.show;
+
+  this.pass.getdata().subscribe(res=>{
+        this.dt=res;
+        for (var i = 0; i < this.dt.length; i++) {
+          if (this.dt[i].id == asd) {
+            this.upd=this.dt[i]
+            console.log("upd",this.upd)
+        }
+      }
+ 
+    })
+
+}
 
 }
